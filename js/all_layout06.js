a2bc0ee7bebc439e8fb3794d4c9c5c10de44285a
jaeee <72128840+jaeeedev@users.noreply.github.com>
2022-03-18 15:24:10 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -11,8 +11,52 @@ $(function () {
 
 $(".visualSlide").slick(
     {
-        arrows: false,
+        arrows: true,
         autoplay: true,
         dots: true,
+        pauseOnHover:false,
+        prevArrow: "<div class='msLeft'></div>",
+        nextArrow: "<div class='msRight'></div>"
     }
 );
+
+
+
+//슬라이드 애니메이션
+$(".visualSlide").on("afterChange", function(e,s,c) {
+    $(".visualSlide figure").eq(c+1).addClass("oo").siblings().removeClass("oo");
+    if (c === 1) {
+        $(".msLeft").addClass("oo")
+    } else {
+        $(".msLeft").removeClass("oo")
+    }
+})
+
+
+$(".eProductSlide").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+})
+
+$(".eventProduct i:nth-of-type(1)").on("click", function(){
+    $(".eProductSlide").slick("slickPause");
+}) 
+$(".eventProduct i:nth-of-type(2)").on("click", function(){
+    $(".eProductSlide").slick("slickPlay");
+}) 
+
+
+$(".aProductSlide").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+});
+
+$(".allProduct i:first-child").on("click", function() {
+    $(".aProductSlide").slick("slickPrev");
+})
+
+$(".allProduct i:last-child").on("click", function() {
+    $(".aProductSlide").slick("slickNext");
+})
